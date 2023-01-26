48d049dc95cd0171fd4b44651b746ef762a52993
aarena18 <ambrearena@gmail.com>
2023-01-26 16:50:38 +0100

    :technologist:

--- backend/script.js
+++ backend/script.js
@@ -1,21 +1,25 @@
+///////////////// IMPORT /////////////////////
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { Server } from "socket.io";
 import http from "http";
-
+import mongoose from "mongoose";
 import router from "./routes/user.js";
+
+///////////////// CONST /////////////////////
 
 const app = express();
 const httpServer = http.createServer(app);
-
 const port = 3000;
-
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
   },
 });
+
+///////////////// SOCKET /////////////////////
 
 io.on("connection", (socket) => {
   console.log(`user connected. Socket id: ${socket.id}`);
@@ -25,27 +29,30 @@ io.on("connection", (socket) => {
   });
 });
 
+///////////////// DB /////////////////////
+
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+///////////////// MIDDELWARE /////////////////////
+
 app.use(bodyParser.json());
 app.use(cors());
+
+///////////////// ROUTES /////////////////////
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/api/user", router);
-
-//app.get("/school", (req, res) => res.send(JSON.stringify({ msg: "IIM" })));
-
-//app.get("/work", (req, res) => res.json("lol"));
-
-//app.post("/", (req, res) => res.json({ msg: "POST" }));
 
 app.post("/", (req, res) => {
   console.log(req.body);
   res.json({ msg: "POST" });
 });
 
-//app.put("/", (req, res) => res.json({ msg: "PUT" }));
-
-//app.delete("/", (req, res) => res.json({ msg: "DELETE" }));
+///////////////// LISTEN /////////////////////
 
 httpServer.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
