00e099bfb9d2754584e3f71b87e14bd9bbd6c25b
ChampiEZ <adam.ammar@edu.devinci.fr>
2023-01-29 22:13:44 +0100

    :tada: Finish my part & :bug: fixed all bugs

--- adam/backend/server.js
+++ adam/backend/server.js
@@ -13,8 +13,7 @@ app.use(express.urlencoded ({extended : true }));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     next();
 });
 
